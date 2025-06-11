35c8b101ae60bd4ceb86da49b0ef1a83ec864c7f
Tharungr2002 <tharungr2002@gmail.com>
2025-06-11 22:05:33 +0530

    Configurations

---
+++ src/main/java/com/example/patientManagementSystem/Model/Patient.java
@@ -0,0 +1,39 @@
+package com.example.patientManagementSystem.Model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true)
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate regiteredDate;
+}
